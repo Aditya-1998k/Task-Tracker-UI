633c2a70b97844b4f6b53127d9266b92a5924496
aditya <aditya98gupta@gmail.com>
2025-10-28 09:46:18 +0530

    minimal setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import './App.css'
 
 function App() {
   return (
     <>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <p>Hello</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
